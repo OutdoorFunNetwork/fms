06f6cc70c30c3b7b67375b9473979a2fae7ea757
Pat McGhen <patrickmcghen@gmail.com>
2022-01-27 15:24:44 -0500

    Update seeder to provide more content for testing.

--- sql/seeder.sql
+++ sql/seeder.sql
@@ -7,9 +7,9 @@ INSERT INTO users(email, password, active)
 
 INSERT INTO user_info(user_id, display_name, location, avatar, bio, primary_activity)
   VALUES
-  (1, 'Eric', 'Pittsburgh, PA', 'https://static.wikia.nocookie.net/p__/images/c/c0/Philip-J-Fry.png/revision/latest?cb=20200507145206&path-prefix=protagonist', 'Eric rides his bike sometimes', 'Bike'),
-  (2, 'Pat', 'Pittsburgh, PA', 'https://upload.wikimedia.org/wikipedia/en/a/a6/Bender_Rodriguez.png', 'Pat loves riding his bike', 'Bike'),
-  (3, 'B the cat', 'Pat''s Couch', 'https://static.wikia.nocookie.net/futuramaworldsoftomorrow_gamepedia_en/images/1/1f/Nibbler.png/revision/latest/scale-to-width-down/250?cb=20170702210232', 'B likes sleeping', 'Sitting on the couch');
+  (1, 'Eric', 'Pittsburgh, PA', 'https://res.cloudinary.com/outdoorfunnetwork/image/upload/v1636261759/eric_ueigl6.jpg', 'Eric rides his bike sometimes', 'Bike'),
+  (2, 'Pat', 'Pittsburgh, PA', 'https://res.cloudinary.com/outdoorfunnetwork/image/upload/v1636261754/pat_kqyl9b.jpg', 'Pat loves riding his bike', 'Bike'),
+  (3, 'B the cat', 'Pat''s Couch', 'https://upload.wikimedia.org/wikipedia/en/a/a6/Bender_Rodriguez.png', 'B likes sleeping', 'Sitting on the couch');
 
 INSERT INTO categories(name)
   VALUES
@@ -20,9 +20,9 @@ INSERT INTO categories(name)
 
 INSERT INTO posts(slug, title, body, author_id, published_at)
   VALUES
-    ('first-post', 'First Post', '# First Post', 1, NOW()),
+    ('first-post', 'First Post', '# First Post\r\nHere are some adventure ideas I had.\r\n  1. Ride bikes\r\n  2. Drink beer\r\nWhat do you think? Should we do those things?', 1, NOW()),
     ('b-post', 'B makes a post', '## B uses headings!', 3, NOW()),
-    ('pat-goes-backpacking', 'Pat goes backpacking', '# Heading 1', 2, NOW());
+    ('pat-goes-backpacking', 'Pat goes backpacking', '# Heading 1\r\nIf rubbin frozen dirt in your crotch is wrong, hey I dont wanna be right. I had more, but you go ahead. You know, I was God once. And Id do it again! And perhaps a third time! But that would be it.\r\nThats not soon enough! Throw her in the brig. Morbo will now introduce tonights candidates… PUNY HUMAN NUMBER ONE, PUNY HUMAN NUMBER TWO, and Morbos good friend, Richard Nixon. You guys realize you live in a sewer, right?\r\n## I videotape every customer that comes in here, so that I may blackmail them later.', 2, NOW());
 
 INSERT INTO posts_categories(post_id, category_id)
   VALUES
